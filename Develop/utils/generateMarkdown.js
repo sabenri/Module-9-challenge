e33655682182d4377269ce5ad1d655bba571d67a
Sabely Sabely <enriquezsabely@gmail.com>
2024-08-28 18:21:33 -0400

    added the function for license badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'Apache License 2.0') {
+    return "https://img.shields.io/badge/Apache%20License%202.0-blue";
+  }
+  else if (license === 'MIT License') {
+    return "https://img.shields.io/badge/MIT%20License-yellow"
+  }
+  else if (license === 'Eclipse Public License') {
+    return "https://img.shields.io/badge/Eclipse%20Public%20License%202.0-lightblue";
+  }
+  else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
