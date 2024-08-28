008d11ec474b14053a3e87e9a18da3a633be6131
Sabely Sabely <enriquezsabely@gmail.com>
2024-08-28 18:39:06 -0400

    added function for the license section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -29,9 +29,12 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license){
+    return'';
+    return 'this project is licensed with ${license}',
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
